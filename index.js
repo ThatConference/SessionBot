e28af7941c950b649ce63cf5b27c05003fd78947
Clark Sell <clark.sell@csell.net>
2017-04-21 12:04:03 -0500

    refactor to make awesome

--- index.js
+++ index.js
@@ -1,107 +1,73 @@
 require('dotenv').config()
-
 const Request = require('request')
-const logger = require('./lib/logger')
 
 const SLACK_URL = process.env.SLACK_URL
-logger.debug(`SLACK_URL: ${SLACK_URL}`)
+console.log(`SLACK_URL: ${SLACK_URL}`)
 
 const headers = {
     'Content-Type': 'application/json'
 }
 
-let options = {
+const options = {
     url: SLACK_URL,
     method: 'POST',
     headers: headers,
-    body: ''
+    timeout: 5000
 }
 
-let postPayload = {
+const postPayload = {
   channel: '#session_updates',
   username: 'ThatBot',
-  text: '',
   icon_emoji: ':that:'
 }
 
 exports.sessionUpdated = (req, res) => {
-  logger.info(`Session Updated Message Called`)
+  console.log(`Session Updated Message Called`)
 
-  return Promise.resolve()
-   .then(() => {
-     if (req.method !== 'POST') {
-       const error = new Error('Only POST requests are accepted');
-       error.code = 405;
+  if (req.method !== 'POST') {
+    const error = new Error('Only POST requests are accepted')
 
-       logger.error(`Non Post Method Recieved: ${req.method}`)
-       throw error;
-     }
+    console.log(`Non Post Method Recieved: ${req.method}`)
 
-      const message = req.body
-      const slackMessage = buildSlackMessage(message)
-      logger.debug(`Formatted Slack Message: \n ${JSON.stringify(slackMessage)}`)
+    return res.status(405).send(error)
+  }
 
-      postPayload.text = slackMessage.text
-      postPayload.attachments = slackMessage.attachments
+  const message = req.body
+  const slackMessage = buildSlackMessage(message)
+  const payload = Object.assign({}, slackMessage, postPayload)
+  const reqOpts = Object.assign({ body: JSON.stringify(payload) }, options)
 
-      //logger.info(`Slack Payload: ${JSON.stringify(postPayload)}`)
-      options.body = JSON.stringify(postPayload)
+  console.log(`Formatted Slack Message: \n ${JSON.stringify(payload)}`)
 
-      //logger.info(`Sending Message To Slack`)
-      Request(options, (error, response, body) => {
-        logger.debug(`Slack Response Code: ${response.statusCode}`)
-        logger.debug(`Request Body Recieved: \n ${body}`)
-
-       if (!error && response.statusCode == 200) {
-          logger.info(`Message Relayed To Slack`)
-
-          res.status(response.statusCode)
-          res.send('Message Relayed to Slack');
-         }
-         res.end();
-      })
-
-    })
-    .catch((err) => {
-      logger.error(err);
-      const code = err.code || (err.response ? err.response.statusCode : 500) || 500;
-      res.status(code).send(err);
-      return Promise.reject(err);
-    });
+  Request(reqOpts).pipe(res)
 }
 
 exports.sessionCancelled = (req, res) => {
-
 }
 
-const buildSlackMessage = (sessionUpdated) => {
-
-  let slackMessage = {
-    text: `*${sessionUpdated.Speaker}'s* session was just updated. :tada:`,
-    attachments: [
-      {
-        fallback: `${sessionUpdated.Speaker} session was just updated.`,
-        color: '#36a64f',
-        author_name: sessionUpdated.Speaker,
-        author_link: sessionUpdated.SpeakerUrl,
-        title: sessionUpdated.Title,
-        title_link: sessionUpdated.URL,
-        text: sessionUpdated.ShortDescription,
-        fields: [
-              {
-                  title: 'Room',
-                  value: sessionUpdated.Room,
-                  short: true
-              },
-              {
-                  title: 'Time',
-                  value: sessionUpdated.Time,
-                  short: true
-              }
-          ]
-      }
-    ]
-  }
-
-  return slackMessage
-}
+const buildSlackMessage = (sessionUpdated) => ({
+  text: `*${sessionUpdated.Speaker}'s* session was just updated. :tada:`,
+  attachments: [
+    {
+      fallback: `${sessionUpdated.Speaker} session was just updated.`,
+      color: '#36a64f',
+      author_name: sessionUpdated.Speaker,
+      author_link: sessionUpdated.SpeakerUrl,
+      title: sessionUpdated.Title,
+      title_link: sessionUpdated.URL,
+      text: sessionUpdated.ShortDescription,
+      fields: [
+        {
+          title: 'Room',
+          value: sessionUpdated.Room,
+          short: true
+        },
+        {
+          title: 'Time',
+          value: sessionUpdated.Time,
+          short: true
+        }
+      ]
+    }
+  ]
+})
